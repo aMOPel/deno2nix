099582a5c519840772bd3f69310babbd534cf5a0
aMOPel <momoPFL@gmail.com>
2026-02-18 16:05:57 +0100

    add installation test

---
+++ tests/installation/default.nix
@@ -0,0 +1,31 @@
+let
+  # deno 2.6.4
+  pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/4c579d27f4e9ae093e3e0326a0b7bf80e106df1c.tar.gz";
+  }) { };
+  deno2nixSrc = pkgs.nix-gitignore.gitignoreSource [ ] ../../.;
+  # deno2nixSrc = pkgs.fetchFromGithub {
+  #   repo = "deno2nix";
+  #   owner = "aMOPel";
+  #   rev = "117817488569489bfe3a4e3ae0b7350206afaed1";
+  #   sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+  # };
+
+  # inject your own nixpkgs
+  deno2nix = import deno2nixSrc { inherit pkgs; };
+in
+deno2nix.lib.buildDenoPackage {
+  pname = "test-deno-build";
+  version = "0.1.0";
+  denoDepsHash = "sha256-RqzZHvDflga7fAz2GrSy27FiZkfgqZE6jpsvoS986I8=";
+  src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
+  denoTaskSuffix = ">out.txt";
+  extraTaskFlags = [
+    "--text"
+    "installation-test"
+  ];
+  installPhase = ''
+    deno --version >>out.txt
+    cp out.txt $out
+  '';
+}
