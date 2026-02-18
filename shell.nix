6476408a71df10dac489acd851e3cdb75b3e7a4e
aMOPel <momoPFL@gmail.com>
2026-02-18 20:59:39 +0100

    move shells

--- shell.nix
+++ shell.nix
@@ -1,9 +1,13 @@
 let
-  sources = import ../lon.nix;
+  sources = import ./lon.nix;
   pkgs = import sources.nixpkgs { };
 in
 pkgs.mkShell {
-  buildInputs = [ pkgs.deno ];
+  buildInputs = with pkgs; [
+    deno
+    gnumake
+    lon
+  ];
   DENO_DIR = "./.deno";
 
   shellHook = '''';
